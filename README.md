3cd27c1874fd610a47131053ca68a5dbacd13fb7
S Anand <root.node@gmail.com>
2023-02-27 20:17:52 +0800

    Create summary of powers with levels

--- README.md
+++ README.md
@@ -4,6 +4,32 @@ Marvel rates character power on a [6-power grid](https://marvel.fandom.com/wiki/
 intelligence, strength, speed, durability, energy projection, and fighting skills.
 
 [scraper.py](scraper.py) scrapes this dataset from [Marvel Fandom](https://marvel.fandom.com/wiki/Category:Characters_by_Power_Grid)
-and saves it as a [CSV file](marvel.csv).
+and saves it as 2 CSV files:
+
+1. [`marvel-powers.csv`](marvel-powers.csv) has 1 row per person and power combination. The columns are:
+   - `name`: Character name, e.g. `Bruce Banner (Earth-616)`
+   - `url`: URL to the character's page, e.g. `https://marvel.fandom.com/wiki/Bruce_Banner_(Earth-616)`
+   - `power`: Power name, e.g. `Strength`
+   - `value`: Power value, e.g. `Normal`
+   - `level`: Power level, e.g. `2`
+2. [`marvel-powers-summary.csv`](marvel-powers-summary.csv) has 1 row per person. The columns are:
+   - `name`: Character name, e.g. `Bruce Banner (Earth-616)`
+   - `url`: URL to the character's page, e.g. `https://marvel.fandom.com/wiki/Bruce_Banner_(Earth-616)`
+   - `min Durability`: Minimum character durability level
+   - `min Energy_Projection`: Minimum character energy projection level
+   - `min Fighting_Skills`: Minimum character fighting skills level
+   - `min Intelligence`: Minimum character intelligence level
+   - `min Speed`: Minimum character speed level
+   - `min Strength`: Minimum character strength level
+   - `max Durability`: Maximum character durability level
+   - `max Energy_Projection`: Maximum character energy projection level
+   - `max Fighting_Skills`: Maximum character fighting skills level
+   - `max Intelligence`: Maximum character intelligence level
+   - `max Speed`: Maximum character speed level
+   - `max Strength`: Maximum character strength level
+
+Each character may have multiple power levels. For example,
+[Anthony Stark (Earth-616)](https://marvel.fandom.com/wiki/Anthony_Stark_(Earth-616)#Powers)
+has a strength level of 2 (Normal) without the Iron Man suit, and a strength level of 6 (Superhuman - 75-100 tons) with it.
 
 The latest update is as of 27 Feb 2023. The dataset contains 2,899 characters, scraping ~113 pages.
--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@
 # https://marvel.fandom.com/wiki/Power_Grid
 # This application scrapes it into a CSV file with columns: name, url, power, value
 
-import csv
+import pandas as pd
 import requests
 from lxml.html import fromstring
 
@@ -12,11 +12,18 @@ def get_power_grid():
     response = requests.get(url)
     tree = fromstring(response.content)
     links = tree.cssselect("#mw-content-text a")
+    last_power, last_value, level = None, None, 0
     for link in links:
         href = link.get("href", "")
         if href.startswith("/wiki/Category:Power_Grid/") and href.count("/") == 4:
             parts = href.split("/")
-            yield {"value": link.text_content(), "power": parts[3], "url": href}
+            power, value = parts[3], parts[4]
+            if power == last_power:
+                if value != last_value:
+                    last_value, level = value, level + 1
+            else:
+                last_power, last_value, level = power, level, 1
+            yield {"value": value, "power": power, "url": href, "level": level}
 
 
 def get_powers():
@@ -33,6 +40,7 @@ def get_powers():
                     "url": link.get("href", ""),
                     "value": item["value"],
                     "power": item["power"],
+                    "level": item["level"],
                 }
             next_link = tree.cssselect(".category-page__pagination-next")
             if len(next_link) == 0:
@@ -42,11 +50,18 @@ def get_powers():
 
 
 def scrape():
-    with open("marvel.csv", "w", encoding="utf-8", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=["name", "url", "power", "value"])
-        writer.writeheader()
-        for item in get_powers():
-            writer.writerow(item)
+    data = pd.DataFrame(get_powers())
+    data["url"] = "https://marvel.fandom.com" + data["url"]
+    data.to_csv("marvel-powers.csv", index=False)
+    powers = data.pivot_table(
+        index=["name", "url"],
+        columns=["power"],
+        values="level",
+        aggfunc=["min", "max"],
+        sort=True,
+    )
+    powers.columns = [" ".join(col).strip() for col in powers.columns.values]
+    powers.reset_index().to_csv("marvel-powers-summary.csv", index=False)
 
 
 if __name__ == "__main__":
